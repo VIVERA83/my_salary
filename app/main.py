b207ce0cea157f7e507d04661af36fafdc45be8a
vivera83 <vivera83@yandex.ru>
2023-07-18 22:22:28 +0300

    fix

--- app/main.py
+++ app/main.py
@@ -1,15 +1,14 @@
 """Модуль запуска приложения."""
 import uvicorn
 
-from core.app import setup_app
 from core.settings import Settings
+from core.app import setup_app
 
 app = setup_app()
-
 if __name__ == "__main__":
     settings = Settings()
-    uvicorn.run(app=app,
+    uvicorn.run(app=setup_app(),
                 host=settings.app_host,
                 port=settings.app_port,
-                workers=settings.app_uvicorn_workers,
+                workers=1,
                 )
